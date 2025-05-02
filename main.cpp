c6a37f4ebdbd549a3b5859f8aa46364359cfef5f
ArcticW1nds <152031200+ArcticW1nds@users.noreply.github.com>
2025-05-02 08:04:59 -0600

    working on the main
    basic layout

--- main.cpp
+++ main.cpp
@@ -13,10 +13,16 @@ int main {
     return 0;
 }
 
-void stringManipulation() {
-	std::string str = "Hello, World!";
-	std::cout << str.length() << std::endl; // Length of the string
-	std::cout << str.substr(0, 5) << std::endl; // Substring from index 0 to 5
-	std::cout << str.find("World") << std::endl; // Find the index of "World"
-	std::cout << str.replace(7, 5, "C++") << std::endl; // Replace "World" with "C++"
+void takeMazeTurn() {
+	//turn
+	//rat moves
+	//check if rat is out of maze
+	// 
+	//scientist gains money
+	//scietist does one of two actions
+	//place owl(air strike)
+	//place cheese(force rat to move toward it)
+	//
+
+
 }
