7ce9d424b8ca8da02c25dc938c3af16166adf864
ArcticW1nds <152031200+ArcticW1nds@users.noreply.github.com>
2025-05-05 08:43:13 -0600

    Restructureing the whole project
    added correct HPP and methods in CPP, implemented some of the combat system.

---
+++ maze.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include "maze.hpp"
+
+
+const int rows = 10;
+const int cols = 11;
+
+
+// 1 = Wall
+// 0 = Path
+// 2 = Start
+// 3 = Exit
+int maze1[rows][cols] = {
+	{1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1},//
+	{1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1},//
+	{1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1},//
+	{1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1},//
+	{1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},//
+	{1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1},//
+	{1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},//
+	{1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1},//
+	{1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1},//
+	{1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1} //
+};
+
+void printMaze(int maze[rows][cols]) {
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			if (maze[i][j] == 1) {
+				std::cout << "#"; // Wall
+			}
+			else if (maze[i][j] == 0) {
+				std::cout << " "; // Path
+			}
+			else if (maze[i][j] == 3) {
+				std::cout << "E"; // Exit
+			}
+		}
+		std::cout << std::endl;
+	}
+}
--- maze.hpp
+++ maze.hpp
@@ -1,47 +1,7 @@
 #ifndef MAZE_HPP
 #define MAZE_HPP
 
-#include <iostream>
-#include <vector>
+extern int maze1[10][11]; // Declare, not define
+void printMaze(int maze[10][11]);
 
-
-
-const int rows = 10;
-const int cols = 11;
-
-
-// 1 = Wall
-// 0 = Path
-// 2 = Start
-// 3 = Exit
-int maze1[rows][cols] = {
-	{1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1},//
-	{1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1},//
-	{1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1},//
-	{1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1},//
-	{1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},//
-	{1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1},//
-	{1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},//
-	{1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1},//
-	{1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1},//
-	{1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1} //
-};	
-
-void printMaze(int maze[rows][cols]) {
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < cols; ++j) {
-			if (maze[i][j] == 1) {
-				std::cout << "#"; // Wall
-			}
-			else if (maze[i][j] == 0) {
-				std::cout << " "; // Path
-			}
-			else if (maze[i][j] == 3) {
-				std::cout << "E"; // Exit
-			}
-		}
-		std::cout << std::endl;
-	}
-}
-
-#endif // MAZE_HPP
+#endif
